86c3fba89c6e3fca66cb44b082e8bf2aa54e1fd8
Jannes Blume <1903180@uad.ac.uk>
2021-10-29 22:53:32 +0100

    feat(gui): provide fx container with concrete types

--- cmd/gui/main.go
+++ cmd/gui/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"assetman/pkg/storage"
+	"assetman/pkg/storage/sqlite"
+	"assetman/pkg/vulnerability"
+	"fmt"
 	"github.com/joho/godotenv"
 	"go.uber.org/fx"
+	"os"
 )
 
 func main() {
@@ -12,7 +17,15 @@ func main() {
 		panic(err)
 	}
 
+	repo := sqlite.NewRepository()
+	vuln := vulnerability.NewService(os.Getenv("NVD_API_URL"), os.Getenv("NVD_API_KEY"))
+
 	app := fx.New(
+		fx.Provide(
+			func() storage.AssetRepository { return repo },
+			func() vulnerability.Service { return vuln },
+		),
+
 		fx.Invoke(InitGui),
 	)
 
