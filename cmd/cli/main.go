66e3b9d360948ca2b389bd6522c27abe58e46d1e
Jannes Blume <1903180@uad.ac.uk>
2021-12-07 16:30:26 +0000

    fix: empty cli main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/cmp307/assetman/pkg/storage/sqlite"
-)
-
 func main() {
-	db, err := sqlite.Connect("C:\\Users\\Jannes\\ScottishGlen\\assets.db")
 
-	if err != nil {
-		panic(err)
-	}
-
-	repo := sqlite.NewAssetRepository(db)
-
-	ass := repo.GetById(1)
-
-	fmt.Println(ass)
 }
